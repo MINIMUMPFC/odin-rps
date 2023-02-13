844619babf2e6f7ae299f73341a3a7f58d0a15a3
Timo Salminen <minimum.pfc@gmail.com>
2023-02-13 11:56:04 +0200

    feat: implement playRound function.

--- script.js
+++ script.js
@@ -10,8 +10,30 @@ const getComputerChoice = function() {
     } else {
         computerChoice = "Scissors";
     };
-    console.log(computerChoice);
     return computerChoice;
 };
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && (computerSelection !== "rock" || computerSelection !== "paper")) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    }
+}
+
+const getPlayersChoice = function() {
+    const playerChoice = prompt("What is your pick? Rock, Paper or Scissors?");
+    const choiceToSmall = playerChoice.toLowerCase();
+
+    //Check if the answer is valid.
+    //if (choiceToSmall !== "rock" || choiceToSmall !== "scissors" || choiceToSmall !== "paper") {
+    //};
+
+    return choiceToSmall;
+}
+
+
+
+const playerSelection = getPlayersChoice();
+//const computerSelection = getComputerChoice();
+const computerSelection = "scissors";
+ 
+console.log(playRound(playerSelection, computerSelection));
